7e3f3cff8e915691caf27527bc950a26cddd0c76
shilvan <5.shilvandacosta@gmail.com>
2021-04-22 13:58:53 +0100

    comment attributes and run method

--- src/main/java/uk/ac/ljmu/fet/cs/csw/CompetitiveMinesweeper/base/solvers/CSW6Solver.java
+++ src/main/java/uk/ac/ljmu/fet/cs/csw/CompetitiveMinesweeper/base/solvers/CSW6Solver.java
@@ -12,24 +12,33 @@ import uk.ac.ljmu.fet.cs.csw.CompetitiveMinesweeper.base.Spot;
 import uk.ac.ljmu.fet.cs.csw.CompetitiveMinesweeper.base.solvers.onepriority.CoordinatesForSpot;
 
 public class CSW6Solver extends AbstractSolver {
-	
+	/**
+	 * Variable used to determine if in looping through all the spots there was an action taken
+	 *to know if to keep looping or pick a new spot to continue the game
+	 */
 	public boolean actionTaken = false;
+	
+	/**
+	 * A spot that can have the potential of having a low probability of being a mine, to use
+	 *instead of picking a spot at random
+	 */
 	CoordinatesForSpot spotToPickWithUncertainty = null;
+	//The probability of that spot being a mine
 	float originalProbability = 1;
 	
+	/**
+	 * The algorithm starts with a point at random and then iterate through all the spots 
+	 * that have i mines around, starting at 1 and ending at 8
+	 */
 	@Override
 	public void run() {
-		
-		// two boilerplate lines that is a strong requirement for almost all solvers
+		// Run and get map to solve
 		super.run();
-		
-		//Get map
 		MineMap myMap = getMyMap(); 
 		
 		//Iterate while the game is not ended
 		mainLoop: do {
-			
-			//Pick a spot at random to start
+			//Pick spot that has a lower probability of being a mine
 			if (originalProbability <= 0.5) {
 				myMap.pickASpot(spotToPickWithUncertainty.rowCoord, spotToPickWithUncertainty.colCoord);
 				System.out.println("\n>>>>>>SPOT PICKED USING PROBABILITY " + originalProbability + " spot: (" + spotToPickWithUncertainty.rowCoord + "," + spotToPickWithUncertainty.colCoord +")");
@@ -38,42 +47,54 @@ public class CSW6Solver extends AbstractSolver {
 				doFullAreaRandomPick(myMap);
 			}
 			
-			
+			//exit loop if the spot picked was a mine
 			if(myMap.isEnded())
 				break mainLoop;
 
-			//Iterate through the spots that have the same nearMine count
+			//Iterate through the spots that have the same nearMine count while actions are being taken
 			do {
+				//Set action taken to false before it enters the loop which will only be able to set it to true or leave unmodified
 				actionTaken = false;
 				
+				//A spot can have 1 through 8 mines around
 				for(int i = 1; i < 9; i++) {
-					
-					searchNearMineCount(myMap, i);
-					
-					if(myMap.isEnded())
-						break mainLoop;
-						
+					System.out.println("\n-NEAR MINE COUNT " + i + ": " );
+					//Find all the spots that have i mines around
+					for (int cc = 0; cc < myMap.cols; cc++) {
+						for (int rc = 0; rc < myMap.rows; rc++) {
+							ExploredSpot aSpot = myMap.getPos(rc, cc);
+							if (Spot.SAFE.equals(aSpot.type)) {
+								if (aSpot.nearMineCount == i) {
+									System.out.print("(" + rc + ", " + cc +"): ");
+									//Search around the spot that has i mines around
+									searchAroundCentre(myMap, new CoordinatesForSpot(rc, cc));
+									//Break out the loop if the last action taken has ended the game
+									if (myMap.isEnded())
+										break mainLoop;
+								}
+							}
+						}
+					}	
 				}
-				
-				
 			} while (actionTaken);
-			
-			
-			
-			
-			
 			
 		} while(!myMap.isEnded());
 		
+		//Print if the game ended with a win or loss
 		if(myMap.isWon()) {
 			System.out.println("\nYOU WON!!!!");
 		} else {
 			System.out.println("\nYOU LOST, EXITING!!!!");
 		}
 		
-		
+		return;
 	}
 	
+	
+	
+
+
+
 	
 	public void searchAroundCentre(MineMap myMap, CoordinatesForSpot centre) {
 		
@@ -117,7 +138,6 @@ public class CSW6Solver extends AbstractSolver {
 			} else if (flaggedSpots.size() == myMap.getPos(centre.rowCoord, centre.colCoord).nearMineCount) {
 				System.out.println("Already Flagged, pick all the spots around it (flagged spots: " + flaggedSpots.size() + ")");
 				pickAllOnList(myMap, unexploredSpots);//pick all unexplored spots if not equal 0
-				
 				actionTaken = true;
 				
 			} else {
@@ -194,26 +214,5 @@ public class CSW6Solver extends AbstractSolver {
 	
 	
 	
-	public void searchNearMineCount(MineMap aMap, int nearMineCount) {
-		
-		
-		System.out.println("\n-NEAR MINE COUNT " + nearMineCount + ": " );
-		List<CoordinatesForSpot> fullList = new ArrayList<CoordinatesForSpot>();
-		for (int cc = 0; cc < aMap.cols; cc++) {
-			for (int rc = 0; rc < aMap.rows; rc++) {
-				ExploredSpot aSpot = aMap.getPos(rc, cc);
-				if (Spot.SAFE.equals(aSpot.type)) {
-					if (aSpot.nearMineCount == nearMineCount) {
-						fullList.add(new CoordinatesForSpot(rc, cc));
-						System.out.print("(" + rc + ", " + cc +"): ");
-						
-						//SEARCH AROUND SPOT AND TAKE ACTIONS
-						searchAroundCentre(aMap, new CoordinatesForSpot(rc, cc));
-					}
-				}
-			}
-		}
-		
-		return;
-	}
+	
 }
